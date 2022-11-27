4f797309f6eab647bf20fd7726a61d52c98c96e4
asdiops <119213071+asdiops@users.noreply.github.com>
2022-11-27 19:56:32 +0300

    Отрисованные спрайты игроков и мяча

--- ping-pong.py
+++ ping-pong.py
@@ -4,6 +4,34 @@ window = display.set_mode((700, 500))
 
 clock = time.Clock()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, speed, picture, x, y):
+        super().__init__()
+        self.speed = speed
+        self.image = transform.scale(image.load(picture), (80, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def __init__(self, speed, picture, x, y):
+        super().__init__(speed, picture, x, y)
+        self.image = transform.scale(image.load(picture), (80, 160))
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def __init__(self, speed, picture, x, y):
+        super().__init__(speed, picture, x, y)
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player1 = Player(5, 'tennis_racket.png', 20, 200)
+player2 = Player(5, 'tennis_racket.png', 600, 200)
+ball = Ball(8, 'png_tennis_ball.png', 350, 250)
+
 game = True
 while game:
     for e in event.get():
@@ -11,6 +39,9 @@ while game:
             game = False
 
     window.fill((220, 232, 112))
+    player1.reset()
+    player2.reset()
+    ball.reset()
 
     clock.tick(60)
     display.update()
